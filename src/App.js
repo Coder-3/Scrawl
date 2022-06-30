fedc77d27ad100f900e978a94ac07aafb3d7815c
Cédric <cedric@liquid-state.com>
2022-06-30 04:13:39 +0000

    Initial config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { RichTextEditor } from "@mantine/rte";
+import { AppShell, Navbar, Header } from "@mantine/core";
+
+const initialValue =
+  "<p>Your initial <b>html value</b> or an empty string to init editor without value</p>";
 
 function App() {
+  const [value, onChange] = useState(initialValue);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppShell
+        padding="md"
+        navbar={
+          <Navbar width={{ base: 300 }} height={500} p="xs">
+            {/* Navbar content */}
+          </Navbar>
+        }
+        header={
+          <Header height={60} p="xs">
+            Notes
+          </Header>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[8]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <RichTextEditor value={value} onChange={onChange} />
+      </AppShell>
     </div>
   );
 }
